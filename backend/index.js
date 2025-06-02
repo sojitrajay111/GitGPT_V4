9e9d1db25c773369187bd2c0a2635e61f3cb9327
raj-patel-149 <rdpatel11124@gmail.com>
2025-06-02 16:04:44 +0530

    make changes in index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://gitgpt-v2.onrender.com",
     credentials: true,
   })
 );
