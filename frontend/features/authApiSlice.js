f7707bd5bfc396c0b6c8a10d8e4cf9d7897225d7
Raj Patel <160699662+raj-patel-149@users.noreply.github.com>
2025-06-03 11:31:59 +0530

    Update authApiSlice.js

--- frontend/features/authApiSlice.js
+++ frontend/features/authApiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const authApiSlice = createApi({
   reducerPath: "authApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://gitgpt-v2.onrender.com/api",
+    baseUrl: "http://localhost:3001/api",
     credentials: "include",
   }),
   tagTypes: ["Auth"],
