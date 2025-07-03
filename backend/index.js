9af0de297694d84d094d24a5f480440a608dc376
umang519 <umangprajapati19504@gmail.com>
2025-07-03 15:38:04 +0530

    login_issue

--- backend/index.js
+++ backend/index.js
@@ -32,6 +32,7 @@ app.use(
       "http://localhost:3002",
       "http://localhost:3003",
       "https://git-gpt-v2.vercel.app",
+      "https://gitgpt-v3.vercel.app"
     ],
     credentials: true,
   })
